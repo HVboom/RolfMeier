2d18ee15c767a882a1273a575a61431259eb21ee
Develop <develop@bimba.local>
2012-11-17 18:07:41 +0100

    allow only 2 level menus

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -13,11 +13,17 @@ class Menu < ActiveRecord::Base
 
   validates_presence_of :title
   validates_uniqueness_of :title, :case_sensitive => false
+  validate :only_2_menu_levels
 
   # enable history
   has_paper_trail
 
   private
+    def only_2_menu_levels
+      # the parent is itself not a child and there is and parent
+      errors.add(:parent, "only 2 levels are allowed") unless (self.parent.nil? || self.parent.root?)
+    end
+
     def strip_whitespaces
       self.title.strip! unless self.title.blank?
     end
