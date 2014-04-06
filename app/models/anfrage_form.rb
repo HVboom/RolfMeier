aec21e41fc61f7c4a1a0a9385a4287b3414e12b7
HVboom <GitHub@HVboom.ch>
2014-04-06 21:59:21 +0200

    Finetune e-Mail Formular

--- app/models/anfrage_form.rb
+++ app/models/anfrage_form.rb
@@ -11,8 +11,8 @@ class AnfrageForm
   attr_accessor :email
   attr_accessor :mitteilung
 
-  validates_presence_of :nachname, :message => 'Bitte Ihren Namen eingeben'
-  validates_presence_of :email, :message => 'Bitte Ihre E-Mail Adresse eingeben'
+  validates_presence_of :nachname, :message => 'Bitte geben Sie Ihren Namen ein'
+  validates_presence_of :email, :message => 'Wir benötigen Ihre E-Mail Adresse'
   validates_format_of :email, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i, :message => 'Bitte eine gültige E-Mail Adresse eingeben'
   validates_presence_of :mitteilung, :message => 'Bitte schreiben Sie eine Mitteilung'
   validates_length_of :mitteilung, :maximum => 500, :message => 'Bitte beschränken Sie sich auf 500 Zeichen'
